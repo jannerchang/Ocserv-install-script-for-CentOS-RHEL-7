3c1141f4fa840c0e5bc4d90efb5d3a625d9ec0ca
jannerchang <janner@ChangJannerdeMacBook-Pro.local>
2015-03-24 10:51:26 +0800

    更新到最新的0.10.1
    更新到最新的0.10.1

--- ocserv-install-script-for-centos7.sh
+++ ocserv-install-script-for-centos7.sh
@@ -36,7 +36,7 @@ function ConfigEnvironmentVariable {
     #最大连接数，默认是16
     maxclients=1024
     #ocserv版本
-    version=${1-0.8.9}
+    version=${1-0.10.1}
     #服务器的证书和key文件，放在本脚本的同目录下，key文件的权限应该是600或者400
     servercert=server-cert.pem
     serverkey=server-key.pem
